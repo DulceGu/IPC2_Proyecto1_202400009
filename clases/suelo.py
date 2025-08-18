2df22768edb39ba51fc3d9914a0fcfc8b73a7e37
DulceGu <3888496650101@ingenieria.usac.edu.gt>
2025-08-17 22:46:51 -0600

    estructura y clases

---
+++ clases/suelo.py
@@ -0,0 +1,8 @@
+class Suelo:
+    def __init__(self, humedad, temperatura, salinidad, conductividad, nutrientes, ph):
+        self.humedad = humedad
+        self.temperatura = temperatura
+        self.salinidad = salinidad
+        self.conductividad = conductividad
+        self.nutrientes = nutrientes
+        self.ph = ph
